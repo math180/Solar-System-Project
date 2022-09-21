939484120878f844c9c5aa8823b27f1472e50a66
Matheus <matheusaraujo.lopes02092016@gmail.com>
2022-09-21 16:33:02 -0300

    finishing

--- src/components/SolarSystem.jsx
+++ src/components/SolarSystem.jsx
@@ -1,12 +1,16 @@
 import React from 'react';
 import Title from './Title';
-import planets from '../data/planets';
 import PlanetCard from './PlanetCard';
+import planets from '../data/planets';
 
 class SolarSystem extends React.Component {
   render() {
     const planetList = planets.map((planet) => (
-      <PlanetCard key={ planet.name } name={ planet.name } image={ planet.image } />
+      <PlanetCard
+        key={ planet.name }
+        planetName={ planet.name }
+        planetImage={ planet.image }
+      />
     ));
 
     return (
