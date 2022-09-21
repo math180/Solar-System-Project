bb8da46b830ead98a6017c291348206443034882
Matheus <matheusaraujo.lopes02092016@gmail.com>
2022-09-21 13:23:26 -0300

    corrigindo requisito 1

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,13 +1,13 @@
 import React from 'react';
 
 class Header extends React.Component {
-    render() {
-        return (
-            <>
-            <h1>Sistema Solar</h1>
-            </>
-        )
-    }
+  render() {
+    return (
+      <header>
+        <h1>Sistema Solar</h1>
+      </header>
+    );
+  }
 }
 
 export default Header;
