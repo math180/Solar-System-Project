485ac4cf62b98957ce3229627f1aeea83f849915
Matheus <matheusaraujo.lopes02092016@gmail.com>
2022-09-21 15:23:22 -0300

    requisito 6

--- src/components/SolarSystem.jsx
+++ src/components/SolarSystem.jsx
@@ -1,11 +1,18 @@
 import React from 'react';
 import Title from './Title';
+import Planets from '../data/planets';
+import PlanetCard from './PlanetCard';
 
 class SolarSystem extends React.Component {
   render() {
+    const planetList = Planets.map((planet) => (
+      <PlanetCard key={ planet.name } name={ planet.name } image={ planet.image } />
+    ));
+
     return (
       <div data-testid="solar-system">
         <Title headline="Planetas" />
+        { planetList }
       </div>
     );
   }
